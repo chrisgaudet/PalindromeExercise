9322b16a971d881ee1117bf98bf7936c1e850f09
Chris Gaudet <chrisgaudet9@gmail.com>
2024-09-19 20:00:55 -0500

    completed palindrome exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            if (string.IsNullOrEmpty(word))
+            {
+                return false;
+            }
+
+            var reversed = "";
+
+            for(int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+
+            if(word.ToLower() == reversed.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,33 @@
+using PalindromeExercise;
 using System;
+using System.Security.Cryptography.X509Certificates;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+       
+        [Theory]
+        [InlineData("dad", true)]
+        [InlineData("level", true)]
+        [InlineData("rotator", true)]
+        [InlineData("civic", true)]
+        [InlineData("hello", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
 
+        public void PalindromeTest(string word, bool expected)
+        {
+            //Arrange
+            var wordSmith = new WordSmith();
+
+            //Act
+            var actual = wordSmith.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
+        
     }
 }
